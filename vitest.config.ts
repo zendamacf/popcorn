2a22335a33ecd11766c423e7e338dc006aedd439
Zach Lang <zach.d.lang@gmail.com>
2026-07-31 23:48:26 +1200

    Adds initial pass of test coverage

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'json-summary'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.d.ts', 'src/**/*.test.ts'],
+      reportsDirectory: './coverage',
+    },
+  },
+});
